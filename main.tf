f701241bae77513c0def97e32be817e7e04224e3
Adnan Grifat <grifat1978@gmail.com>
2026-03-27 15:19:48 -0500

    add ec2 web server

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_instance" "web_server" {
+  ami                         = "ami-05024c2628f651b80"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "smartops-web-server"
+  }
+}
