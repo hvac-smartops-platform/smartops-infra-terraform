2948995076e48171bbc29327ac489ac7cf0ce944
Adnan Grifat <grifat1978@gmail.com>
2026-03-27 20:18:41 -0500

    add second web server

--- main.tf
+++ main.tf
@@ -23,6 +23,27 @@ resource "aws_instance" "web_server" {
   }
 }
 
+resource "aws_instance" "web_server_2" {
+  ami                         = "ami-05024c2628f651b80"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Welcome to SmartOps Platform - Server 2</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "smartops-web-server-2"
+  }
+}
+
 resource "aws_eip" "web_ip" {
   domain   = "vpc"
   instance = aws_instance.web_server.id
